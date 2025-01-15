6ac3ad9eb47bfaa8783cd4321858a7230b2a4b90
Rahim Alizada <rahim.alizada@gmail.com>
2025-01-15 11:56:20 +0400

    feat: adds level to Profiled annotation (#63)

--- src/main/java/com/jyvee/spring/methodprofiler/aspect/ProfiledAdvice.java
+++ src/main/java/com/jyvee/spring/methodprofiler/aspect/ProfiledAdvice.java
@@ -22,7 +22,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.slf4j.event.Level;
 import org.springframework.stereotype.Component;
 
 import java.text.NumberFormat;
@@ -56,11 +55,13 @@ public class ProfiledAdvice {
         if (this.properties.getEventPublishingEnabled()) {
             event.commit();
         }
-        if (this.properties.getLoggingEnabled() && log.isEnabledForLevel(Level.INFO)) {
-            log.info("{}.{}: {} ms",
-                point.getTarget().getClass().getSimpleName(),
-                point.getSignature().getName(),
-                NumberFormat.getNumberInstance(Locale.US).format(System.currentTimeMillis() - startMillis));
+        if (this.properties.getLoggingEnabled() && log.isEnabledForLevel(profiled.value().getLevel())) {
+            log
+                .atLevel(profiled.value().getLevel())
+                .log("{}.{}: {} ms",
+                    point.getTarget().getClass().getSimpleName(),
+                    point.getSignature().getName(),
+                    NumberFormat.getNumberInstance(Locale.US).format(System.currentTimeMillis() - startMillis));
         }
         return object;
     }
